979e1e58dc3231d12b475d1d580520906aa85db9
LotuxPunk <clement@vandendaelen.com>
2018-12-01 22:35:15 +0100

    Initial commit

---
+++ src/main/java/com/giftsunderthetree/GiftsUnderTheTree.java
@@ -0,0 +1,29 @@
+package com.giftsunderthetree;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+
+public class GiftsUnderTheTree {
+    public static final String MODID = "examplemod";
+    public static final String NAME = "Example Mod";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
